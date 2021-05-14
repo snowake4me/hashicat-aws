38343c89dba9b8fc3e1543868e88e34285f75834
Billy Glenn <snowake4me@gmail.com>
2021-05-14 19:35:46 +0000

    Fix variable syntax

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,5 +4,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/bdg3-training/s3-bucket/aws"
   version = "2.1.0"
   # insert required variables here
-  bucket_prefix = "$prefix"
+  bucket_prefix = var.prefix
 }
